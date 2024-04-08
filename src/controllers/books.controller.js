bc7c6268aa46f12bd4200246dd71c36b49929104
Cristian Devia <c.devia8@gmail.com>
2024-04-08 17:20:36 -0500

    add book controller methods

---
+++ src/controllers/books.controller.js
@@ -0,0 +1,7 @@
+export const createBooks = (req, res) => {};
+export const getBooks = (req, res) => {
+  res.json("get books list");
+};
+export const getBooksById = (req, res) => {};
+export const updateBooksById = (req, res) => {};
+export const deleteBooksById = (req, res) => {};
