7a4b17a93b2668a11cddb36e556ca1231164a3ee
Cristian Devia <c.devia8@gmail.com>
2024-04-08 22:42:57 -0500

    add db name

--- src/database.js
+++ src/database.js
@@ -4,8 +4,9 @@ import mongoose from "mongoose";
 const USER = process.env.MONGOATLAS_USER;
 const PASSWORD = process.env.MONGOATLAS_PASSWORD;
 const DB = process.env.MONGOATLAS_DB;
-const APPNAME = process.env.MONGOAPPNAME;
-const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${DB}.5wjlgjq.mongodb.net/?retryWrites=true&w=majority&appName=${APPNAME}`;
+const APPNAME = process.env.MONGO_APP_NAME;
+const DB_NAME = process.env.MONGO_DB_NAME;
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${DB}.5wjlgjq.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=${APPNAME}`;
 mongoose
   .connect(MONGO_URI)
   .then(() => console.log("Connection Successful"))
