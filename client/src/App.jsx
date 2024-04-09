d758fbe20e081d71ec62caaacdd3663f7edbfb97
Cristian Devia <c.devia8@gmail.com>
2024-04-09 16:58:10 -0500

    set routes for front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,19 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
-function App(){
-  return(
+function App() {
+  return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<h1>Home Page</h1>} />
-      <Route path='/login' element={<h1>Login</h1>} />
-      <Route path='/register' element={<h1>Register</h1>} />
-      <Route path='/books' element={<h1>Book List</h1>} />
-      <Route path='/add-book' element={<h1>Add Book</h1>} />
-      <Route path='/book/:id' element={<h1>Book ID</h1>} />
-
-    </Routes>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/books" element={<h1>Book List</h1>} />
+        <Route path="/add-book" element={<h1>Add Book</h1>} />
+        <Route path="/book/:id" element={<h1>Book ID</h1>} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
-export default App
+export default App;
