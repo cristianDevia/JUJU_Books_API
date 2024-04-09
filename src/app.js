7c230a3792d1eefede8137643d1d9747676f4e07
Cristian Devia <c.devia8@gmail.com>
2024-04-09 10:29:55 -0500

    set routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import booksRoutes from "./routes/books.routes";
+import authRoutes from "./routes/auth.routes";
 import "./database";
 
 const app = express();
 app.use(express.json());
-app.use("/books", booksRoutes);
+
+app.use("/api/books", booksRoutes);
+app.use("/api/auth", authRoutes);
 
 export default app;
