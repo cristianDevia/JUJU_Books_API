c70144ecc578d3fce2240513f579eaf8ece62178
Cristian Devia <c.devia8@gmail.com>
2024-04-09 10:29:03 -0500

    create signin and signup methods

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,58 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+import config from "../config";
+
+export const signUp = async (req, res) => {
+  try {
+    if (req && req.body) {
+      const { email, password } = req.body;
+
+      const newUser = new User({
+        email,
+        password: await User.encryptPassword(password),
+      });
+
+      const savedUser = await newUser.save();
+
+      const token = jwt.sign({ id: savedUser._id }, config.signatureToken, {
+        expiresIn: 86400, // 24 hours = 86400 seconds
+      });
+
+      res.json({ token });
+    }
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+export const signIn = async (req, res) => {
+  try {
+    if (req && req.body) {
+      const { email, password } = req.body;
+      const userExist = await User.findOne({ email });
+
+      if (userExist) {
+        const matchPassword = await User.comparePassword(
+          password,
+          userExist.password
+        );
+
+        if (matchPassword) {
+          const token = jwt.sign({ id: userExist._id }, config.signatureToken, {
+            expiresIn: 86400,
+          });
+
+          res.json({ token });
+        } else {
+          res
+            .status(401)
+            .json({ token: null, message: "Contraseña incorrecta" });
+        }
+      } else {
+        res.status(400).json({ message: "Usuario no encontrado" });
+      }
+    }
+  } catch (error) {
+    res.json(error);
+  }
+};
