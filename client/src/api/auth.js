bb8319ea4a997d01dd21f9cf0166e26e5d0c8901
Cristian Devia <c.devia8@gmail.com>
2024-04-09 17:34:37 -0500

    set variable

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-
-const API_BASE_URL = "http://localhost:3000/api";
+import { BASE_URL } from "../../config";
 
 export const registerUser = (user) => {
-  return axios.post(`${API_BASE_URL}/auth/signup`, user);
+  return axios.post(`${BASE_URL}/auth/signup`, user);
 };
