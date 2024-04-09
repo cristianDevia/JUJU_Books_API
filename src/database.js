a1edba5a63b9568e22c772cebf037f762f1007f7
Cristian Devia <c.devia8@gmail.com>
2024-04-09 10:31:29 -0500

    change mongo url with config variables

--- src/database.js
+++ src/database.js
@@ -1,11 +1,7 @@
-require("dotenv").config();
 import mongoose from "mongoose";
+import config from "./config";
 
-const USER = process.env.MONGOATLAS_USER;
-const PASSWORD = process.env.MONGOATLAS_PASSWORD;
-const DB = process.env.MONGOATLAS_DB;
-const APPNAME = process.env.MONGO_APP_NAME;
-const DB_NAME = process.env.MONGO_DB_NAME;
+const { USER, PASSWORD, DB, DB_NAME, APPNAME } = config;
 const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${DB}.5wjlgjq.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=${APPNAME}`;
 mongoose
   .connect(MONGO_URI)
