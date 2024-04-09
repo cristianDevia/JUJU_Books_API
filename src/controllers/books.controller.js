65f53aea8416664702ef8f9eea9f3b31fb4d2515
Cristian Devia <c.devia8@gmail.com>
2024-04-08 22:44:43 -0500

    add api methods

--- src/controllers/books.controller.js
+++ src/controllers/books.controller.js
@@ -1,7 +1,57 @@
-export const createBooks = (req, res) => {};
-export const getBooks = (req, res) => {
-  res.json("get books list");
+import Book from "../models/Book";
+
+export const createBooks = async (req, res) => {
+  try {
+    if (req && req.body) {
+      const { title, author, publicationYear, state } = req.body;
+      const newBook = new Book({
+        title,
+        author,
+        publicationYear,
+        state,
+      });
+
+      const bookSaved = await newBook.save();
+      res.json(bookSaved);
+    }
+  } catch (error) {
+    res.json(error);
+  }
 };
-export const getBooksById = (req, res) => {};
-export const updateBooksById = (req, res) => {};
-export const deleteBooksById = (req, res) => {};
+
+export const getBooks = async (req, res) => {
+  try {
+    const booksList = await Book.find();
+    res.json(booksList);
+  } catch (error) {
+    res.json(error);
+  }
+};
+export const getBooksById = async (req, res) => {
+  try {
+    const findBookById = await Book.findById(req.params.bookId);
+    res.json(findBookById);
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+export const updateBooksById = async (req, res) => {
+  const updatedBook = await Book.findByIdAndUpdate(
+    req.params.bookId,
+    req.body,
+    {
+      new: true,
+    }
+  );
+  res.json(updatedBook);
+};
+
+export const deleteBooksById = async (req, res) => {
+  try {
+    const deletedBook = await Book.findByIdAndDelete(req.params.bookId);
+    res.json(deletedBook);
+  } catch (error) {
+    res.json(error);
+  }
+};
